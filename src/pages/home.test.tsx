c5eea8606a8c378e66ba67cb353b68786319a31e
Ryan Mitchell <ryanem@hotmail.co.uk>
2020-12-27 11:52:50 +0000

    update

--- src/pages/home.test.tsx
+++ src/pages/home.test.tsx
@@ -3,7 +3,7 @@ import Home from "./home";
 import { render } from "@testing-library/react";
 
 describe("Home", () => {
-  it("should render default component", () => {
+  it("should render default compon", () => {
     const { asFragment } = render(<Home />);
 
     expect(asFragment()).toMatchSnapshot();
