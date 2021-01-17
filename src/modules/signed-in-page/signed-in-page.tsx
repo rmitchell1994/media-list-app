ca246c9cfa6389fac0eb643e04a7266a89e00935
Ryan Mitchell <ryanem@hotmail.co.uk>
2021-01-17 16:20:57 +0000

    Fix unit tests

--- src/modules/signed-in-page/signed-in-page.tsx
+++ src/modules/signed-in-page/signed-in-page.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 
 const SignedInPage: FC = () => {
-  return <div data-testid="signed-in-wrapper">Signed In Pag</div>;
+  return <div data-testid="signed-in-wrapper">Signed In Page</div>;
 };
 
 export default SignedInPage;
